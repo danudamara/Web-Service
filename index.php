7d69d24cad403d9e18381489c87753c4c6d37caa
danudamara <48472598+danudamara@users.noreply.github.com>
2019-03-12 15:50:59 +0700

    Add files via upload
    m. danu damara
    16.01.53.0123

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
+<h1>Hello World</h1> <br/><table>
     <tr>
         <td>NIM</td>
         <td> : </td>
-        <td>16.01.53.0064</td>
+        <td>16.01.53.0123</td>
     </tr>
     <tr>
         <td>Nama</td>
         <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
+        <td>M. Danu Damara</td>
     </tr>
-</table> <br>";
-
-    echo $text;
-?>
-
-    <a href="/akademik">Go To Akademik</a>
+</table>
+<a href="/akademik">Go To Akademik</a>
